f8eb3bd4a1eb53a0bd54480f50e3f333a4e2ddff
acerohernan <contacto.acero.hernan@gmail.com>
2023-07-14 19:28:02 -0500

    build: Init the cdk repository

---
+++ README.md
@@ -0,0 +1,3 @@
+# Goland pub sub AWS CDK integration
+
+This is a testing project to implement an event driven architecture with AWS CDK and golang
---
+++ pub-sub-aws.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"os"
+
+	"github.com/aws/aws-cdk-go/awscdk/v2"
+	"github.com/aws/constructs-go/constructs/v10"
+	"github.com/aws/jsii-runtime-go"
+)
+
+type PubSubAwsStackProps struct {
+	awscdk.StackProps
+}
+
+func NewPubSubAwsStack(scope constructs.Construct, id string, props *PubSubAwsStackProps) awscdk.Stack {
+	var sprops awscdk.StackProps
+	if props != nil {
+		sprops = props.StackProps
+	}
+	stack := awscdk.NewStack(scope, &id, &sprops)
+
+	return stack
+}
+
+func main() {
+	defer jsii.Close()
+
+	app := awscdk.NewApp(nil)
+
+	NewPubSubAwsStack(app, "PubSubAwsStack", &PubSubAwsStackProps{
+		awscdk.StackProps{
+			Env: env(),
+		},
+	})
+
+	app.Synth(nil)
+}
+
+func env() *awscdk.Environment {
+
+	return &awscdk.Environment{
+		Account: jsii.String(os.Getenv("CDK_DEFAULT_ACCOUNT")),
+		Region:  jsii.String(os.Getenv("CDK_DEFAULT_REGION")),
+	}
+}
